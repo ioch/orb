98e00d1bbdfbd74491b978207e1ba34f883b1798
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-16 20:12:04 +0300

    Automatic commit at 2015-08-16 20:12:04

--- orb.ino
+++ orb.ino
@@ -117,8 +117,8 @@ void reschedule() {
 void read_control() {
     static int last_duty = analogRead(POT_LEFT);
     int duty = analogRead(POT_LEFT);
-    float dutyf = duty / 1024.0;
     if (duty != last_duty) {
+        float dutyf = duty / 1024.0;
         off_delay = (unsigned long)(on_delay * dutyf);
         reschedule();
     }
