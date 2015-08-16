418c19725fa981960859606f2aa6b09b6ab21b45
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-16 20:14:06 +0300

    Automatic commit at 2015-08-16 20:14:06

--- orb.ino
+++ orb.ino
@@ -83,6 +83,8 @@ void off() {
 unsigned long last_on = 0;
 unsigned long off_delay = 42;
 unsigned long on_delay = 42;
+unsigned long next_on = 0;
+unsigned long next_off = 0;
 
 void reschedule() {
     next_off = last_on + off_delay;
