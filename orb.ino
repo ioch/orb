7e9dba527409a17134baec28cd1850409e4f630d
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-16 20:32:35 +0300

    Automatic commit at 2015-08-16 20:32:35

--- orb.ino
+++ orb.ino
@@ -134,7 +134,7 @@ void read_control() {
     static unsigned long last_tap = 0;
     static bool last_button = 0;
     bool button = digitalRead(BUTTON);
-    if (button > last_button) {
+    if (button) {
         on();
         delay(42);
         off();
