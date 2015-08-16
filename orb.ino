a12df0c3ec6e0d8ac5a4f9f7e4501d8c57352fef
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-16 20:43:30 +0300

    Automatic commit at 2015-08-16 20:43:30

--- orb.ino
+++ orb.ino
@@ -144,7 +144,7 @@ void read_control() {
     last_duty = duty;
 
     saturation = analogRead(POT_MIDDLE) / 4;
-    brighness = analogRead(POT_RIGHT) / 4;
+    brightness = analogRead(POT_RIGHT) / 4;
 
     static unsigned long last_tap = 0;
     static bool last_button = 0;
