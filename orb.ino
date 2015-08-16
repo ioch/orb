6678a624c4f60840c1abe419f85be85067d7b049
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-16 20:54:56 +0300

    Automatic commit at 2015-08-16 20:54:56

--- orb.ino
+++ orb.ino
@@ -153,7 +153,7 @@ void read_control() {
         unsigned long delay = millis() - last_tap;
         last_tap = millis();
         if (delay < MAX_DELAY) {
-            on_delay = delay / 4;
+            on_delay = delay / 2;
             reset_off();
             reschedule();
         }
